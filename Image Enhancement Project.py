b090bdbba441a83a3637c0f7710399cd8c226076
Kenny-skv <102988868+Kenny-skv@users.noreply.github.com>
2025-02-05 20:56:42 +0000

    Update Image Enhancement Project.py

--- Image Enhancement Project.py
+++ Image Enhancement Project.py
@@ -1,4 +1,5 @@
-!pip install OpenCV-python
+!pip install opencv-python
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,10 +10,10 @@ from google.colab import files
 # Load a low-contrast grayscale image
 image = cv2.imread('low contrast image.png', cv2.IMREAD_GRAYSCALE)
 
-# Check if the image is loaded
-if image is None:
-    print("Error: Image not found or path is incorrect!")  # More informative error message
-    exit()
-
 # 1. Histogram Equalization
 hist_eq_image = cv2.equalizeHist(image)
+
+# 2. CLAHE
+# Create a CLAHE object with desired parameters
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+clahe_image = clahe.apply(image)
