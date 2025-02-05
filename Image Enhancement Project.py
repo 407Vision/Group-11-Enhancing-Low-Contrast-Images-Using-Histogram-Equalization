7ce4beb2b103499df48efe25b0ceab4ef926f7e0
Aminatest <102568790+Aminatest@users.noreply.github.com>
2025-02-05 20:06:03 +0000

    Update Image Enhancement Project.py

--- Image Enhancement Project.py
+++ Image Enhancement Project.py
@@ -1,11 +1,18 @@
-from google.colab import files
-import matplotlib.pyplot as plt
-import numpy as np
+!pip install OpenCV-python
 import cv2
-!pip install opencv-python
+import numpy as np
+import matplotlib.pyplot as plt
+from google.colab import files
 
-
-# uploaded = files.upload()
+#uploaded = files.upload()
 
 # Load a low-contrast grayscale image
-image = cv2.imread('low contrast image.png' cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('low contrast image.png', cv2.IMREAD_GRAYSCALE)
+
+# Check if the image is loaded
+if image is None:
+    print("Error: Image not found or path is incorrect!")  # More informative error message
+    exit()
+
+# 1. Histogram Equalization
+hist_eq_image = cv2.equalizeHist(image)
